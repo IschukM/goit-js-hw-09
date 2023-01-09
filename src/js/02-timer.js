a7f3f23b24357083fc344b77fb74171c2bccff98
IschukM <dzubak@ukr.net>
2023-01-09 14:01:59 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -24,7 +24,6 @@ function convertMs(ms) {
 date = new Date();
 dateInput = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button');
-// console.log(startBtn);
 const dataDays = document.querySelector('[data-days]');
 const dataHours = document.querySelector('[data-hours]');
 const dataMinutes = document.querySelector('[data-minutes]');
